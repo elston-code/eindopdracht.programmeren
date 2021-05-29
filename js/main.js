43b0d8d781f27a33526cef5f1de6a5dfe935595c
elston-code <giovanni1942@live.nl>
2021-05-29 15:39:11 +0200

    functie toegevoegd dieren

--- js/main.js
+++ js/main.js
@@ -12,18 +12,40 @@
 // }
 
 
-var hoeden = document.querySelectorAll("#hoeden button");
-var dieren = document.querySelectorAll("#hoeden button");
+var susimg = document.querySelector("#mainbody")
+var btns = document.querySelectorAll(".pakje")
 
-var susimg = document.querySelector("#classhoed")
+var hoeden = document.querySelectorAll("#hoeden button");
 var hoedimg = document.querySelector("#classhoed")
 
+var dieren = document.querySelectorAll("#dieren button");
+var dierimg = document.querySelector("#classpet")
+
+console.log(susimg)
+
+ 
+
+function changeColor(event){
+    console.log(susimg.src);
+    if(susimg.src == event.target.src){
+        // speelt geluid af
+        // De image is al geklikt en dan krijg je dus een melding dat het al geklikt is.
+        console.log('je hebt deze kleur al gekozen');
+
+    } else {
+        // btns.src = event.target.src
+        console.log("nu heb je deze kleur gekozen")
+        // console.log(event.target.src);
+        susimg.src = event.target.src
+    }
+}
+
 function changeHoed(event){
-    console.log(event);
-    console.log(hoedimg.src);
-    console.log(event.target.src);
+    // console.log(event);
+    // console.log(hoedimg.src);
+    // console.log(event.target.src);
     if(hoedimg.src == event.target.src){
-        // speel geluid af
+        // speelt geluid af
         console.log('je hebt deze hoed al op');
     } else {
         console.log(event.target.dataset);
@@ -34,11 +56,32 @@ function changeHoed(event){
     }
 }
 
+function changeDier(event){
+    
+    if(dierimg.src == event.target.src){
+        // speelt geluid af
+        console.log('je hebt deze hoed al op');
+    } else {
+        console.log(event.target.dataset);
+        dierimg.style.right = event.target.dataset.right
+        dierimg.style.bottom = event.target.dataset.bottom
+        dierimg.style.width = event.target.dataset.width
+        dierimg.src = event.target.src
+    }
+}
+
+
+// functies linken aan variabele
+
+btns.forEach(btn =>{
+    btn.addEventListener("click",changeColor);
+})
 
 hoeden.forEach(hoed => {
     hoed.addEventListener("click", changeHoed);
 });
 
 dieren.forEach(dier => {
-    dier.addEventListener("click", changeHoed);
+    dier.addEventListener("click", changeDier);
 });
+
